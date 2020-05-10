b20fe62622f1e1fd341779bbdb70b9611e951327
Ashlen Mencia <ashmencia0103@gmail.com>
2020-05-10 00:21:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,6 +47,12 @@ class Song
     self.all.sort_by { |song| song.name[0]}
   end
 
+  def self.new_from_filename(name)
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
 
 
 
